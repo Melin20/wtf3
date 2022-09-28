adfd8c09cf4973560810ff7d45cb2e2c9f06ab7e
Melin20 <abbaadaeze@gmail.com>
2022-09-28 03:07:30 +0100

    first commit

---
+++ ada.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract Ada{
+
+    //fixed array
+
+    uint[10] cynthia = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//dynamic array. We don't specify the size in dynamic array.
+
+    uint [] daisy = [1,2,3,4,5,6,7,8,10,100,10000000];
+
+//to make your code visible to others, add public
+
+    uint [] public daisy = [1,2,3,4,5,6,10,100];
+
+
+}
